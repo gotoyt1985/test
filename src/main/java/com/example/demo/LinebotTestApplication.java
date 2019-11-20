a40eee82b77719a07069792a4919c5c81e8d19d9
Yuji_takahashi <gotoyt1985@gmail.com>
2019-11-21 00:58:57 +0900

    make it better

--- src/main/java/com/example/demo/LinebotTestApplication.java
+++ src/main/java/com/example/demo/LinebotTestApplication.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.boot.SpringApplication;
@@ -8,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.ImageMessageContent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
@@ -25,7 +27,12 @@ public class LinebotTestApplication {
         System.out.println("event: " + event);
         return new TextMessage(event.getMessage().getText());
     }
-
+    @EventMapping
+    public TextMessage handleImageMessageEvent(MessageEvent<ImageMessageContent> event) throws IOException {
+        System.out.println("event: " + event);
+        return new TextMessage("画像応答");
+    }
+        
     @EventMapping
     public void handleDefaultMessageEvent(Event event) {
         System.out.println("event: " + event);
